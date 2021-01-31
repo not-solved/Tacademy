3b3f15deac4ead65445886504b6e17b8b5d72a0c
not-solved <rudtn5799@naver.com>
2021-01-31 15:41:24 +0900

    AOP example

--- Spring Framework/SpringWebPrj/src/myspring/user/dao/UserMapper.java
+++ Spring Framework/SpringWebPrj/src/myspring/user/dao/UserMapper.java
@@ -1,13 +1,13 @@
 package myspring.user.dao;
-
 import java.util.List;
 import myspring.user.vo.UserVO;
 
+@MyMapper
 public interface UserMapper {
-	
-	void insertUser(UserVO user);
-	void updateUser(UserVO user);
-	void deleteUser(String id);
 	UserVO selectUserById(String id);
 	List<UserVO> selectUserList();
+	void insertUser(UserVO userVO);
+	void updateUser(UserVO userVO);
+	void deleteUser(String id);
 }
+
