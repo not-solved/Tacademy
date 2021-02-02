afcbe93afeb6c5e1e21dd22d61f6a7d9d9ba528f
not-solved <rudtn5799@naver.com>
2021-02-02 14:06:39 +0900

    Spring MVC example

--- Spring Framework/SpringWebPrj/src/myspring/user/controller/UserController.java
+++ Spring Framework/SpringWebPrj/src/myspring/user/controller/UserController.java
@@ -1,9 +1,9 @@
 package myspring.user.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,13 +22,89 @@ import myspring.user.vo.UserVO;
 public class UserController {
 	@Autowired
 	private UserService userService;
-
+	
+	// 데이터베이스 내 등록된 사용자 정보 확인 페이지
 	@RequestMapping("/getUserList.do")
 	public String getUserList(Model model) {
 		List<UserVO> userList = userService.getUserList();
 		model.addAttribute("userList", userList);
 		return "userList";
 	}
-
 	
+	// 사용자 개인의 등록정보 확인 페이지
+	@RequestMapping("/getUser.do")
+	public ModelAndView getUser(@RequestParam String id) {
+		UserVO user = userService.getUser(id);
+		return new ModelAndView("userInfo", "user", user);
+	}
+	
+	// 신규 사용자 등록 페이지
+	@RequestMapping("/insertUserForm.do")
+	public ModelAndView insertUserForm() {
+		List<String> genderList = new ArrayList<String> ();
+		genderList.add("남");
+		genderList.add("여");
+		
+		List<String> cityList = new ArrayList<String> ();
+		cityList.add("서울");
+		cityList.add("부산");
+		cityList.add("인천");
+		cityList.add("대구");
+		cityList.add("제주");
+		cityList.add("대전");
+		
+		Map<String, List<String>> map = new HashMap<>();
+		map.put("genderList", genderList);
+		map.put("cityList", cityList);
+		return new ModelAndView("userInsert", "map", map);
+	}
+
+	// 등록 페이지에서 등록 버튼 누를 시 데이터베이스에 삽입연산이 이루어지는 과정
+	@RequestMapping("/insertUser.do")
+	public String insertUser(@ModelAttribute UserVO user) {
+		if(user != null) {
+			userService.insertUser(user);
+		}
+		return "redirect:/getUserList.do";
+	}
+	
+	@RequestMapping("/updateUserForm.do")
+	public ModelAndView updateUserForm(@RequestParam String id) {
+		UserVO user = userService.getUser(id);
+		
+		List<String> genderList = new ArrayList<String> ();
+		genderList.add("남");
+		genderList.add("여");
+		
+		List<String> cityList = new ArrayList<String> ();
+		cityList.add("서울");
+		cityList.add("부산");
+		cityList.add("인천");
+		cityList.add("대구");
+		cityList.add("제주");
+		cityList.add("대전");
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("genderList", genderList);
+		map.put("cityList", cityList);
+		map.put("user", user);
+		return new ModelAndView("userUpdate", "map", map);
+	}
+	
+	@RequestMapping("/updateUser.do")
+	public String updateUser(@ModelAttribute UserVO user) {
+		userService.updateUser(user);
+		return "redirect:/getUserList.do";
+	}
+	
+	@RequestMapping("/deleteUser.do/{id}")
+	public String deleteUser(@PathVariable String id) {
+		userService.deleteUser(id);
+		return "redirect:/getUserList.do";
+	}
+	
+	@ExceptionHandler
+	public String handleException(Exception e) {
+		return "viewError";
+	}
 }
